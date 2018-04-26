91dee28b56405edff4b275cafeb5ef34bebf5119
Chris Campbell <chris@ctcampbell.com>
2018-04-26 17:52:42 +0100

    Add concurrency

--- veracodestats/data_manager.py
+++ veracodestats/data_manager.py
@@ -1,4 +1,5 @@
 import os
+import concurrent.futures
 import xml.etree.cElementTree as cET
 import veracodestats.model as model
 
@@ -17,23 +18,38 @@ class DataManager:
     def load_data(self, from_folder=None):
         report_filename_list = sorted(os.listdir(from_folder))
         report_filepath_list = [os.path.join(from_folder, report_filename) for report_filename in report_filename_list]
-        report_filepath_count = len(report_filepath_list)
 
-        for index, report_filepath in enumerate(report_filepath_list):
-            print("\rProcessing reports {}/{}".format(index + 1, report_filepath_count), end="")
+        unparsed_reports = []
+        parsed_reports = []
 
-            unparsed_report = self._read_report_file(report_filepath)
-            parsed_report = cET.fromstring(unparsed_report)
+        with concurrent.futures.ThreadPoolExecutor() as executor:
+            count = 0
+            for unparsed_report in executor.map(self._read_report_file, report_filepath_list):
+                count += 1
+                print("\rLoading report files: {}/{}".format(count, len(report_filepath_list)), end="")
+                unparsed_reports.append(unparsed_report)
+            print("")
 
-            if parsed_report.tag == "{https://www.veracode.com/schema/reports/export/1.0}detailedreport":
-                app = self.apps.get(parsed_report.attrib["app_id"], model.App(parsed_report))
-                build = model.Build(parsed_report)
-                if build.sandbox_id:
-                    sandbox = app.sandboxes.get(build.sandbox_id, model.Sandbox(parsed_report))
-                    sandbox.builds.append(build)
-                    app.sandboxes[build.sandbox_id] = sandbox
-                else:
-                    app.builds.append(build)
-                self.apps[app.id] = app
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            count = 0
+            for parsed_report in executor.map(cET.fromstring, unparsed_reports):
+                count += 1
+                print("\rParsing reports: {}/{}".format(count, len(unparsed_reports)), end="")
+                if parsed_report.tag == "{https://www.veracode.com/schema/reports/export/1.0}detailedreport":
+                    parsed_reports.append(parsed_report)
+            print("")
+
+        for index, report in enumerate(parsed_reports):
+            print("\rProcessing reports {}/{}".format(index + 1, len(parsed_reports)), end="")
+
+            app = self.apps.get(report.attrib["app_id"], model.App(report))
+            build = model.Build(report)
+            if build.sandbox_id:
+                sandbox = app.sandboxes.get(build.sandbox_id, model.Sandbox(report))
+                sandbox.builds.append(build)
+                app.sandboxes[build.sandbox_id] = sandbox
+            else:
+                app.builds.append(build)
+            self.apps[app.id] = app
 
         print("")
